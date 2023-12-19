5032aa36413629450ab0502304f1739d401aff95
DimaOstanin <xnadab@gmail.com>
2023-12-19 21:35:03 +0200

    Home page ready

--- backEnd/index.js
+++ backEnd/index.js
@@ -7,12 +7,12 @@ const app = express();
 
 app.use(express.json())
 
-app.use(
-    cors({
-    origin: 'http://localhost:3000',
-    methods:['GET' ,'POST','PUT','DELETE'],
-    allowedHeaders: ['Content-type']
-}))
+// app.use(
+//     cors({
+//     origin: 'http://localhost:3000',
+//     methods:['GET' ,'POST','PUT','DELETE'],
+//     allowedHeaders: ['Content-type']
+// }))
 
 
 app.get('/' ,(request,response)=> {
